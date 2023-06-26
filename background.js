5324baf8320287d9a287906f4905759bc7663487
wisdom nwakaku <nwakakucc@gmail.com>
2023-06-26 09:11:39 +0100

    walkin

--- background.js
+++ background.js
@@ -3,9 +3,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.subtitles) {
     const subtitles = request.subtitles;
 
-    // Store the subtitles in Chrome Storage API
-    chrome.storage.local.set({ translatedSubtitles: subtitles }, function () {
-    });
+    // Pass the subtitles to the popup script
+    chrome.runtime.sendMessage({ subtitles: subtitles });
   }
 });
 
@@ -13,14 +12,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   // Check if the updated tab is a YouTube URL
   if (tab.url && tab.url.includes('youtube.com/watch')) {
-    // Retrieve the stored subtitles from Chrome Storage API
-    chrome.storage.local.get('translatedSubtitles', function (result) {
-      const subtitles = result.translatedSubtitles;
-
-      // Pass the subtitles to the content script
-      chrome.tabs.sendMessage(tabId, { translatedSubtitles: subtitles });
-    });
-
     // Inject content script into the updated tab
     chrome.scripting.executeScript({
       target: { tabId: tabId },
--- popup.js
+++ popup.js
@@ -21,42 +21,60 @@ let subtitleState = {
           subtitles: [...subtitleState.subtitles, ...uniqueWords],
         };
   
-        // Display the subtitle state in the popup
-        displaySubtitles(subtitleState.subtitles);
-  
-        // Speak the subtitle words
-        speakSubtitle(uniqueWords);
+        // Pass the subtitle array as a paragraph to popup.html
+        passSubtitleToPopup(subtitleState.subtitles);
+        console.log(subtitleState.subtitles)
       }
     }
   });
   
-  // Display the subtitle state in the popup
-  function displaySubtitles(subtitles) {
-    const subtitleListElement = document.getElementById('subtitle-list');
-    subtitleListElement.innerHTML = '';
+  // Pass the subtitle text to popup.html
+  async function passSubtitleToPopup(subtitleText) {
+    // Get the subtitle text element in popup.html
+    const subtitleTextElement = document.getElementById('subtitle-text');
   
-    subtitles.forEach((subtitle) => {
-      const subtitleItem = document.createElement('li');
-      subtitleItem.textContent = subtitle;
-      subtitleListElement.appendChild(subtitleItem);
-    });
+    // Join the subtitles into a paragraph with punctuation
+    const paragraph = subtitleText.join(' ') + '.';
+  
+    // Filter, fill missing words, and punctuate the paragraph using ChatGPT API
+    const filteredParagraph = await generateFilteredParagraph(paragraph);
+  
+    // Set the subtitle paragraph
+    subtitleTextElement.textContent = filteredParagraph;
   }
   
-  // Speak the subtitle words using speech synthesis
-  function speakSubtitle(words) {
-    let currentIndex = 0;
+  // Function to filter, fill missing words, and punctuate the paragraph using ChatGPT API
+  async function generateFilteredParagraph(paragraph) {
+    const apiKey = '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'; // Replace with your ChatGPT API key
+    const endpoint = 'https://api.openai.com/v1/chat/completions';
   
-    function speakNextWord() {
-      if (currentIndex < words.length) {
-        const word = words[currentIndex];
-        const utterance = new SpeechSynthesisUtterance(word);
-        speechSynthesis.speak(utterance);
+    // Prepare the request payload
+    const payload = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: 'The input passed here is a continuous subtitle from a video we are currently watching. Fill in missing words and punctuate the paragraph. Update the sentences based on the last words, but do not change the words. Do not complain either.' },
+        { role: 'user', content: paragraph },
+      ],
+      max_tokens: 100,
+      temperature: 0.7,
+      n: 1,
+      stop: '\n',
+    };
   
-        currentIndex++;
-        setTimeout(speakNextWord, 700); // Delay between each word (adjust as needed)
-      }
-    }
+    // Make the API request to ChatGPT API
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(payload),
+    });
   
-    speakNextWord();
+    // Parse the response
+    const data = await response.json();
+    const generatedSentence = data.choices[0].message.content;
+  
+    return generatedSentence;
   }
   
